5b57761223fcd98f9e1bf95bb80661ce375982b0
menard1825 <mike1825@gmail.com>
2026-08-18 10:40:31 -0400

    Load live game clock helper

--- static/js/navigation_v2.js
+++ static/js/navigation_v2.js
@@ -82,6 +82,7 @@
     loadScript('/static/js/game_prep_readiness.js', 'game-readiness');
     loadScript('/static/js/game_pitching_rule_picker.js', 'game-pitching-rules');
     loadScript('/static/js/live_game_sync_status.js', 'live-sync');
+    loadScript('/static/js/live_game_clock.js', 'live-game-clock');
   }
 
   document.querySelectorAll('a[href="#games"] span, a[href="#games"], [data-nav-games]').forEach((el) => {
